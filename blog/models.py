9c49ebc5557ccde88054c26a0d6107d54967bdab
Tim <orcatim@hotmail.com>
2022-06-01 20:31:00 +0000

    add comments and posts admin

--- blog/models.py
+++ blog/models.py
@@ -26,8 +26,9 @@ class Post(models.Model):
         return self.likes.count()
 
 
-class Comments(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name = 'comments')
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -35,7 +36,7 @@ class Comments(models.Model):
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
